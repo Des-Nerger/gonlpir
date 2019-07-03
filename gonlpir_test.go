03e7b46f974ec3b6cd44b8dce958475a41b5c1f1
Des-Nerger <mixerator@rambler.ru>
2019-07-04 01:39:02 +1000

    revert gonlpir_test strings as they were in 9fccb7e14369abdfdccbcd0b6b10c8cfc8afe02d

--- gonlpir_test.go
+++ gonlpir_test.go
@@ -34,8 +34,8 @@ func TestParagraphProcess(t *testing.T) {
 		return
 	}
 	defer nlpir.Exit()
-	fmt.Println("TestParagraphProcess: 我是 ３中国人")
-	fmt.Println(nlpir.ParagraphProcess("我是 ３中国人", true))
+	fmt.Println("TestParagraphProcess: 我是中国人")
+	fmt.Println(nlpir.ParagraphProcess("我是中国人", true))
 }
 
 func TestParagraphProcessA(t *testing.T) {
@@ -45,8 +45,8 @@ func TestParagraphProcessA(t *testing.T) {
 		return
 	}
 	defer nlpir.Exit()
-	fmt.Println("TestParagraphProcessA: == 字幕制作：波士顿 == 你听我的劝，吃点好吗？　")
-	results := nlpir.ParagraphProcessA("== 字幕制作：波士顿 == 你听我的劝，吃点好吗？　", true)
+	fmt.Println("TestParagraphProcessA: 我是中国人")
+	results := nlpir.ParagraphProcessA("我是中国人", true)
 	for i := 0; i < len(results); i++ {
 		fmt.Printf("%#v\n", results[i])
 	}
